f6f63b57719adbbb0f7971ff841a570f7b2df4c9
Rune Sostack Clausen <runesostackclausen@gmail.com>
2023-04-04 21:54:40 +0200

    initial commit

---
+++ src/views/holdView.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  mounted() {
+  }
+});
+</script>
+
+<template>
+  <div class="row my-4">
+    <h1 class="display-5">
+      Aarhus Fremad Atletik!
+    </h1>
+    <h3 class="display-5">Hold</h3>
+    <ul>
+      <li>Løb & Motion</li>
+      <li>Mellem/Lang</li>
+      <li>Spring</li>
+      <li>Længdespring</li>
+      <li>Børneatletik</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/views/rekorderView.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  mounted() {
+  }
+});
+</script>
+
+<template>
+  <div class="row my-4">
+    <h1 class="display-5">
+      Aarhus Fremad Atletik!
+    </h1>
+    <h3 class="display-5">Klub rekorder</h3>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
